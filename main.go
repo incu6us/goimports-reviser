bd2853d8509f157451af35c333b45cb01fbc1fbd
Vyacheslav Pryimak <vyacheslavpryimak@gmail.com>
2020-04-08 22:54:28 +0300

    cleanup

--- main.go
+++ main.go
@@ -1,22 +1,80 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+
+	"errors"
+	"strings"
+
+	"os"
 
 	"github.com/incu6us/goimport-reviser/reviser"
 )
 
 const (
-	projectName = "goimport-reviser"
-	filePath    = "./testdata/example.go"
+	projectNameKey = "project-name"
+	filePathKey    = "file-path"
 )
 
+var projectName, filePath string
+
+func init() {
+	flag.StringVar(
+		&projectName,
+		projectNameKey,
+		"",
+		"your project name(ex.: github.com/incu6us/goimport-reviser/reviser)",
+	)
+
+	flag.StringVar(
+		&filePath,
+		filePathKey,
+		"",
+		"file path to fix imports(ex.: ./reviser/reviser.go)",
+	)
+}
+
+var usage = func() {
+	if _, err := fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0]); err != nil {
+		log.Fatalf("failed to print usage: %s", err)
+	}
+
+	flag.PrintDefaults()
+}
+
 func main() {
+	flag.Parse()
+
+	if err := validateInputs(projectName, filePath); err != nil {
+		fmt.Println(err)
+		usage()
+		os.Exit(1)
+	}
+
 	out, err := reviser.Execute(projectName, filePath)
 	if err != nil {
 		log.Fatalf("%+v", err)
 	}
 
 	fmt.Println(string(out))
+}
+
+func validateInputs(projectName, filePath string) error {
+	var errMesages []string
+
+	if projectName == "" {
+		errMesages = append(errMesages, fmt.Sprintf("%s should be set", projectNameKey))
+	}
+
+	if filePath == "" {
+		errMesages = append(errMesages, fmt.Sprintf("%s should be set", filePathKey))
+	}
+
+	if len(errMesages) > 0 {
+		return errors.New(strings.Join(errMesages, "\n"))
+	}
+
+	return nil
 }
--- testdata/innderpkg/inner.go
+++
@@ -1,7 +0,0 @@
-package innderpkg
-
-import "github.com/pkg/errors"
-
-func InnerFn(s string) (string, error) {
-	return s, errors.New("some error")
-}

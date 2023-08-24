31bb08682f5a64bca3a2487ed27716d4e57a0f1f
meiping <meiping@vip.qq.com>
2023-08-24 14:13:24 +0800

    Fix bug when handling file paths on Windows

--- reviser/file.go
+++ reviser/file.go
@@ -10,7 +10,7 @@ import (
 	"go/token"
 	"io"
 	"os"
-	"path"
+	"path/filepath"
 	"regexp"
 	"sort"
 	"strings"
@@ -460,7 +460,7 @@ func (f *SourceFile) parseImports(file *ast.File) (map[string]*commentsMetadata,
 	var err error
 
 	if shouldRemoveUnusedImports || shouldUseAliasForVersionSuffix {
-		packageImports, err = astutil.LoadPackageDependencies(path.Dir(f.filePath), astutil.ParseBuildTag(file))
+		packageImports, err = astutil.LoadPackageDependencies(filepath.Dir(f.filePath), astutil.ParseBuildTag(file))
 		if err != nil {
 			return nil, err
 		}

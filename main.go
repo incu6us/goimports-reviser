6fc01510ad47bf5e906d3d2bedc2216aebdf13de
Viacheslav Pryimak <vyacheslavpryimak@gmail.com>
2025-02-19 12:26:25 +0200

    fix: set-exit-status on recursivePath (#171)
    * This fixes set-exit-status when using `./...`
    
    https://github.com/incu6us/goimports-reviser/issues/93
    
    * conflict resolve
    
    * conflict resolve
    
    ---------
    
    Co-authored-by: Micael Malta <mmalta@justworks.com>

--- main.go
+++ main.go
@@ -368,13 +368,22 @@ func main() {
 				if err != nil {
 					log.Fatalf("Failed to find unformatted files %s: %+v\n", originPath, err)
 				}
-				fmt.Printf("%s\n", unformattedFiles.String())
-				continue
+
+				if unformattedFiles != nil {
+					fmt.Printf("%s\n", unformattedFiles.String())
+					if *setExitStatus {
+						os.Exit(1)
+					}
+				}
+
+				return
 			}
+
 			err := reviser.NewSourceDir(originProjectName, originPath, *isRecursive, excludes).Fix(options...)
 			if err != nil {
 				log.Fatalf("Failed to fix directory %s: %+v\n", originPath, err)
 			}
+
 			continue
 		}
 
